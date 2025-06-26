33d57a098b4db18427bd3e9a6c1f2568603c03f9
Samuel Schwarz <schwarz@jopixel.cz>
2025-06-26 10:46:43 +0200

    Fix auth token cache - expire_in is in seconds, not minutes

--- PayU.Client/src/PayU.Client/Extensions/CacheObjectExtension.cs
+++ PayU.Client/src/PayU.Client/Extensions/CacheObjectExtension.cs
@@ -32,7 +32,7 @@ namespace PayU.Client.Extensions
                 var communicator = new PayUApiHttpCommunicator<PayUToken>(clientFactory, settings);
                 cachedObject = (T)await communicator.SendAsync(request, ct);
                 var policy = new CacheItemPolicy();
-                policy.AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(cachedObject.ExpireIn - 30);
+                policy.AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(cachedObject.ExpireIn - 30);
                 cache.Set(tokenKey, cachedObject, policy);
             }
 
@@ -58,7 +58,7 @@ namespace PayU.Client.Extensions
                 var communicator = new PayUApiHttpCommunicator<PayUToken>(clientFactory, settings);
                 cachedObject = (T)communicator.Send(request);
                 var policy = new CacheItemPolicy();
-                policy.AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(cachedObject.ExpireIn - 30);
+                policy.AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(cachedObject.ExpireIn - 30);
                 cache.Set(tokenKey, cachedObject, policy);
             }
 
